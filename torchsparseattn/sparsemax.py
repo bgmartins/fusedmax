8d3d0048eee2d46a7ee4953e18c5433d33023519
Bruno Martins <bgmartins@users.noreply.github.com>
2024-07-28 18:31:09 +0100

    Update sparsemax.py

--- torchsparseattn/sparsemax.py
+++ torchsparseattn/sparsemax.py
@@ -6,6 +6,25 @@ import torch
 from torch import nn
 from torch import autograd as ta
 
+def project(v, z=1): 
+    v_sorted, _ = torch.sort(v, dim=0, descending=True)
+    cssv = torch.cumsum(v_sorted, dim=0) - z
+    ind = torch.arange(1, 1 + len(v)).to(dtype=v.dtype)
+    cond = v_sorted - cssv / ind > 0
+    rho = ind.masked_select(cond)[-1]
+    tau = cssv.masked_select(cond)[-1] / rho
+    w = torch.clamp(v - tau, min=0)
+    return w
+    
+def project_jv(dout, w_star):
+    supp = w_star > 0
+    masked = dout.masked_select(supp)
+    nnz = supp.to(dtype=dout.dtype).sum()
+    masked -= masked.sum() / nnz
+    out = dout.new(dout.size()).zero_()
+    out[supp] = masked
+    return(out)  
+
 class SparsemaxFunction(ta.Function):
 
     @staticmethod
@@ -53,28 +72,7 @@ class SparsemaxFunction(ta.Function):
         if lengths is None: lengths = [max_dim] * n_samples
         for i in range(n_samples): din[i, :lengths[i]] = project_jv(dout[i, :lengths[i]], y_star[i, :lengths[i]])
         if requires_squeeze: din = din.squeeze()
-        return din, None
-        
-    @staticmethod
-    def project(v, z=1): 
-        v_sorted, _ = torch.sort(v, dim=0, descending=True)
-        cssv = torch.cumsum(v_sorted, dim=0) - z
-        ind = torch.arange(1, 1 + len(v)).to(dtype=v.dtype)
-        cond = v_sorted - cssv / ind > 0
-        rho = ind.masked_select(cond)[-1]
-        tau = cssv.masked_select(cond)[-1] / rho
-        w = torch.clamp(v - tau, min=0)
-        return w
-    
-    @staticmethod
-    def project_jv(dout, w_star):
-        supp = w_star > 0
-        masked = dout.masked_select(supp)
-        nnz = supp.to(dtype=dout.dtype).sum()
-        masked -= masked.sum() / nnz
-        out = dout.new(dout.size()).zero_()
-        out[supp] = masked
-        return(out)        
+        return din, None      
 
 class Sparsemax(nn.Module):
 
