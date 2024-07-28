81c9c2f4819ad79af2d1119c3f8dd26ba941af6f
Bruno Martins <bgmartins@users.noreply.github.com>
2024-07-28 18:02:19 +0100

    Update sparsemax.py

--- torchsparseattn/sparsemax.py
+++ torchsparseattn/sparsemax.py
@@ -12,32 +12,28 @@ import torch
 from torch import nn
 from .base import _BaseBatchProjection
 
-def project_simplex(v, z=1):
-    v_sorted, _ = torch.sort(v, dim=0, descending=True)
-    cssv = torch.cumsum(v_sorted, dim=0) - z
-    ind = torch.arange(1, 1 + len(v)).to(dtype=v.dtype)
-    cond = v_sorted - cssv / ind > 0
-    rho = ind.masked_select(cond)[-1]
-    tau = cssv.masked_select(cond)[-1] / rho
-    w = torch.clamp(v - tau, min=0)
-    return w
-
-def sparsemax_grad(dout, w_star):
-    supp = w_star > 0
-    masked = dout.masked_select(supp)
-    nnz = supp.to(dtype=dout.dtype).sum()
-    masked -= masked.sum() / nnz
-    out = dout.new(dout.size()).zero_()
-    out[supp] = masked
-    return(out)
-
 class SparsemaxFunction(_BaseBatchProjection):
 
     @staticmethod
-    def project(x): return project_simplex(x)
+    def project(v, z=1): 
+        v_sorted, _ = torch.sort(v, dim=0, descending=True)
+        cssv = torch.cumsum(v_sorted, dim=0) - z
+        ind = torch.arange(1, 1 + len(v)).to(dtype=v.dtype)
+        cond = v_sorted - cssv / ind > 0
+        rho = ind.masked_select(cond)[-1]
+        tau = cssv.masked_select(cond)[-1] / rho
+        w = torch.clamp(v - tau, min=0)
+        return w
     
     @staticmethod
-    def project_jv(dout, y_star): return sparsemax_grad(dout, y_star)
+    def project_jv(dout, w_star):
+        supp = w_star > 0
+        masked = dout.masked_select(supp)
+        nnz = supp.to(dtype=dout.dtype).sum()
+        masked -= masked.sum() / nnz
+        out = dout.new(dout.size()).zero_()
+        out[supp] = masked
+        return(out)        
 
 class Sparsemax(nn.Module):
 
