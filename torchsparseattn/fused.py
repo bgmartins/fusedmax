951375306fc0f6bc2d08f280b62babce75d217ca
Bruno Martins <bgmartins@users.noreply.github.com>
2024-07-28 18:38:27 +0100

    Update fused.py

--- torchsparseattn/fused.py
+++ torchsparseattn/fused.py
@@ -11,8 +11,7 @@ def _inplace_fused_prox_jv_slow(y_hat, dout):
     n_features = len(dout)
     for i in range(n_features + 1):
         if i in (0, n_features) or y_hat[i] != y_hat[i - 1]:
-            if i > 0:
-                dout[last_ix:i] = acc / n
+            if i > 0: dout[last_ix:i] = acc / n
             if i < n_features:
                 last_ix = i
                 acc = dout[i]
@@ -25,8 +24,7 @@ def _inplace_fused_prox_jv_slow(y_hat, dout):
 try:
     from ._fused_jv import _inplace_fused_prox_jv
 except ImportError:
-    warnings.warn("Could not import cython implementation of fused backward "
-                  "pass. Slow implementation used instead.")
+    warnings.warn("Could not import cython implementation of fused backward pass. Slow implementation used instead.")
     _inplace_fused_prox_jv = _inplace_fused_prox_jv_slow
 
 def fused_prox_jv_slow(y_hat, dout):
@@ -60,4 +58,4 @@ class Fusedmax(nn.Module):
     def forward(x, alpha=1, lengths=None):
         fused_prox = FusedProxFunction(alpha)
         sparsemax = SparsemaxFunction()
-        return sparsemax(fused_prox(x, lengths), lengths)
+        return sparsemax.apply(fused_prox.apply(x, lengths), lengths)
