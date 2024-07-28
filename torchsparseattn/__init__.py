02a9b5fa66397a6267f2064ddf012a3fa37dce66
Bruno Martins <bgmartins@users.noreply.github.com>
2024-07-28 19:37:11 +0100

    Update __init__.py

--- torchsparseattn/__init__.py
+++ torchsparseattn/__init__.py
@@ -2,4 +2,4 @@ from .fused import Fusedmax, FusedProxFunction
 from .oscar import Oscarmax, OscarProxFunction
 from .sparsemax import Sparsemax, SparsemaxFunction
 
-__version__ = __VERSION__ = '0.3.dev0'
+__version__ = __VERSION__ = '0.4.dev0'
